a7e3d2253b706b8c50c74fe084ccb970e0a6c6e8
Christina <christinajun113@gmail.com>
2022-08-19 12:20:01 -0400

    working quote machine wo api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
+const quotes = [
+  {quoteText: '“Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.” ', 
+author: 'Michael Scott',},
+{quoteText: '“I never thought I’d say this, but I think I ate too much bone marrow.” ', 
+author: 'Dwight Schrute',},
+{quoteText: '“Me think, why waste time say lot word, when few word do trick.”', 
+author: 'Kevin Malone',}
+]
+
 
 function App() {
+  
+  const [quote, setquote] = useState([]);
+  const [index, setIndex] = useState(0);
+  
+  
+  function changeQuote(index){
+    index===quotes.length-1? setIndex(0): setIndex(prevIndex => prevIndex + 1);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="quote-container">
+          <p>{quotes[index].quoteText}</p>
+          <p className="author-text">-{quotes[index].author}</p>
+          <button className = "change-quote-btn" onClick = {() => changeQuote(index)}>Next Quote</button>
+        </div>
       </header>
     </div>
   );
