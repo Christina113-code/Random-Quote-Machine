2e1aff66444c57e2932fa86b01d367b685200e75
Christina <christinajun113@gmail.com>
2022-08-20 08:34:07 -0400

    quotes now come from Zen Quotes API :)

--- src/App.js
+++ src/App.js
@@ -14,8 +14,18 @@ author: 'Kevin Malone',}
 
 function App() {
   
-  const [quote, setquote] = useState([]);
+  const [quote, setquote] = useState("“Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.”");
   const [index, setIndex] = useState(0);
+  const [author, setAuthor] = useState("Michael Scott");
+  // useEffect(() => {
+  //   fetchData();
+  // });
+  let fetchData = async() => {
+    const res = await fetch("https://zenquotes.io/api/random");
+    const data = await res.json();
+    setquote(data[0].q);
+    setAuthor(data[0].a);
+  }
   
   
   function changeQuote(index){
@@ -26,9 +36,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="quote-container">
-          <p>{quotes[index].quoteText}</p>
-          <p className="author-text">-{quotes[index].author}</p>
-          <button className = "change-quote-btn" onClick = {() => changeQuote(index)}>Next Quote</button>
+          <p>{quote}</p>
+          <p className="author-text">-{author}</p>
+          <button onClick = {() => fetchData()}>Next Quote</button>
         </div>
       </header>
     </div>
